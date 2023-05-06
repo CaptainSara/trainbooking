c34d0b8a7bf8697106efe319a6c3de770a574f25
CaptainSara <sara.iqbal@hotmail.se>
2023-05-06 18:18:17 +0200

    starting up the project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import {Button} from "antd"
+function App() {
+  return (
+    <div className="App m-5">
+      <h1>Train Booking</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
